70be4aa62284aa13c8e0924dbb2f870af9c6a135
Raj Sahu <21je0738@iitism.ac.in>
2024-09-12 08:33:44 +0530

    file paths corrected

--- codes/download.bash
+++ codes/download.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Create directories if they don't exist
-mkdir -p ../data/training_data ../data/test_data
+trainDir="data/training_data"
+testDir="data/test_data"
+mkdir -p $trainDir $testDir
 
 # Function to download, unzip, and clean up
 download_and_extract() {
@@ -24,15 +26,15 @@ download_and_extract() {
 }
 
 # Download, unzip, and clean up training data
-echo "Downloading and extracting training data..."
-for i in $(seq 1 17); do
-	url="https://xeek-public-287031953319-eb80.s3.amazonaws.com/image-impeccable/image-impeccable-train-data-part${i}.zip"
-    download_and_extract "$url" "./training_data/"
-done
+# echo "Downloading and extracting training data..."
+# for i in $(seq 1 17); do
+# 	url="https://xeek-public-287031953319-eb80.s3.amazonaws.com/image-impeccable/image-impeccable-train-data-part${i}.zip"
+#     download_and_extract "$url" $trainDir
+# done
 
 # Download, unzip, and clean up test data
 echo "Downloading and extracting test data..."
 test_url="https://xeek-public-287031953319-eb80.s3.amazonaws.com/image-impeccable/image-impeccable-test-data.zip"
-download_and_extract "$test_url" "./test_data/"
+download_and_extract "$test_url" $testDir
 
 echo "Download, extraction, and cleanup complete!"
